2715b119c426fd8a410cbcca918c143718020beb
Saxaar <kuzmiku99@gmail.com>
2023-01-04 23:14:34 +0700

    delete persons

--- backend/src/main/kotlin/com/kotlinspringvue/backend/controller/BackendController.kt
+++ backend/src/main/kotlin/com/kotlinspringvue/backend/controller/BackendController.kt
@@ -3,13 +3,11 @@ package com.kotlinspringvue.backend.controller
 import org.springframework.web.bind.annotation.*
 import com.kotlinspringvue.backend.model.Greeting
 import java.util.concurrent.atomic.AtomicLong
-import com.kotlinspringvue.backend.repository.PersonRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.Authentication
 import com.kotlinspringvue.backend.repository.UserRepository
 import com.kotlinspringvue.backend.jpa.User
-import com.kotlinspringvue.backend.repository.PerfomanceRepository
 import com.kotlinspringvue.backend.web.response.ResponseMessage
 import org.springframework.http.ResponseEntity
 import org.springframework.http.HttpStatus
@@ -25,8 +23,6 @@ class BackendController() {
 //    @Autowired
 //    lateinit var emailService: EmailServiceImpl
 
-    @Autowired
-    lateinit var personRepository: PersonRepository
 
     @Autowired
     lateinit var userRepository: UserRepository
@@ -37,8 +33,6 @@ class BackendController() {
     fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
             Greeting(counter.incrementAndGet(), "Hello, $name")
 
-    @GetMapping("/persons")
-    fun getPersons() = personRepository.findAll()
 
     @GetMapping("/usercontent")
     @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
